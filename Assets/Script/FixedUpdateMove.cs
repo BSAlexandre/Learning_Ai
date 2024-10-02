cd0bb83a0446d2e4a2bc0271e38b771a9dbbeca6
AlexandreBS <sousabalexandre@gmail.com>
2024-10-02 15:38:34 +0100

    Adams_Walk_not_Finished

---
+++ Assets/Script/FixedUpdateMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedUpdateMove : MonoBehaviour
+{
+    public float spd = 0.01f;
+
+    void FixedUpdate()
+    {
+        this.transform.Translate(0, 0, spd);
+    }
+}
---
+++ Assets/Script/UpdateMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateMove : MonoBehaviour
+{
+   public float spd = 0.01f;
+
+    void Update()
+    {
+        this.transform.Translate(0, 0, spd);
+    }
+}
