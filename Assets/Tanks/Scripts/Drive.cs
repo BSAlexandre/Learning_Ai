54ae06636907b66b196b3db5e6f599e5d81d2556
AlexandreBS <sousabalexandre@gmail.com>
2024-10-03 22:03:27 +0100

    Acelaration 1 Done

--- Assets/Tanks/Scripts/Drive.cs
+++ Assets/Tanks/Scripts/Drive.cs
@@ -6,6 +6,9 @@ public class Drive : MonoBehaviour
 {
     public float speed = 10.0f;
     public float rotationSpeed = 100.0f;
+    public Transform transGun;
+    public Transform gun;
+    public GameObject bulletObj;
 
     void Update()
     {
@@ -24,5 +27,20 @@ public class Drive : MonoBehaviour
 
         // Rotate around our y-axis
         transform.Rotate(0, rotation, 0);
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transGun.RotateAround(transGun.position, transGun.right, -2);
+        }
+        else if (Input.GetKey(KeyCode.E)) 
+        {
+            transGun.RotateAround(transGun.position, transGun.right, 2);
+        }
+        else if (Input.GetKeyDown(KeyCode.F)) 
+        {
+            Instantiate(bulletObj, gun.position, gun.rotation);
+        
+        }
+
     }
 }
